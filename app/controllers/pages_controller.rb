d69b1b0265faf5c8e17659d342ec9074f64c6538
GuzelGokhan <dev.guzel@gmail.com>
2020-06-17 00:22:37 +0200

    stupid coaching

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+    def ask
+    end
+
+    def answer
+        @question = params[:question]
+        if @question.eql?("I am going to work right now!")
+            @message = ""
+        elsif @question.end_with?("?")
+            @message = "Silly question, get dressed and go to work!"
+        else
+            @message = "I don't care, get dressed and go to work!"
+        end
+    end
+end
